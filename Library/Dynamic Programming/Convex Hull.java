a64b530171d319d22dc4f878b2b86208aae6130d
omaryasser <oyaraouf@gmail.com>
2018-03-07 13:25:42 +0200

    Added convex hull code

---
+++ Library/Dynamic Programming/Convex Hull.java
@@ -0,0 +1,79 @@
+public class ConvexHull {
+
+        long[] A, B;            //initialized with size n
+        long[] starts;
+        long[] ends;
+        int ptr, len;
+        long INF = (long)1e10;
+        ConvexHull (int n) {
+            A = new long[n];
+            B = new long[n];
+            starts = new long[n];
+            ends = new long[n];
+        }
+
+        void addLine(long a, long b) // // lines are sorted with their slopes decreasing
+        {
+            while (len >= 2 &&
+                    (A[len - 1] - A[len - 2]) * (B[len - 1] - b) <= (A[len - 1] - a) * (B[len - 1] - B[len - 2]))
+                --len;
+            A[len] = a;
+            B[len] = b;
+            if (len == 0) starts[len] = -INF;
+            else {
+                starts[len] = (B[len] - B[len - 1] + A[len - 1] - A[len] - 1) / (A[len - 1] - A[len]);
+            }
+            ++len;
+        }
+
+        long queryPointer(long x)	// O(n), queries must be performed in increasing order (TODO : Test it)
+        {
+            ptr = Math.min(ptr, len - 1);
+            while(ptr < len - 1 && A[ptr + 1] * x + B[ptr + 1] <= A[ptr] * x + B[ptr])
+                ++ptr;
+            return A[ptr] * x + B[ptr];
+        }
+
+        void addLine2(long a, long b) // lines are sorted with their slopes increasing
+        {
+            while (len >= 2 &&
+                    (A[len - 1] - A[len - 2]) * (B[len - 1] - b) >= (A[len - 1] - a) * (B[len - 1] - B[len - 2]))
+                --len;
+            A[len] = a;
+            B[len] = b;
+            if (len == 0) ends[0] = INF;
+            else {
+                ends[len] = (B[len] - B[len - 1]) / (A[len - 1] - A[len]);
+            }
+            ++len;
+        }
+
+
+        long queryBS(long x) { // use it if you filled starts[]
+            int ans = 0;
+            int lo = 1, hi = len - 1;
+            while (lo <= hi) {
+                int mid = lo + hi >> 1;
+                if (starts[mid] <= x ) {
+                    ans = mid;
+                    lo = mid + 1;
+                } else
+                    hi = mid - 1;
+            }
+            return  x * A[ans] + B[ans];
+        }
+
+        long queryBS2(long x) { // use it if you filled ends[]
+            int ans = 0;
+            int lo = 1, hi = len - 1;
+            while (lo <= hi) {
+                int mid = lo + hi >> 1;
+                if (ends[mid] >= x ) {
+                    ans = mid;
+                    lo = mid + 1;
+                } else
+                    hi = mid - 1;
+            }
+            return  x * A[ans] + B[ans];
+        }
+    }
