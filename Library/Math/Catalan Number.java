0edb5ca943089473b578f2d915fa44fff2ecf711
Omar Yasser <oyaraouf@gmail.com>
2018-03-29 06:25:06 +0200

    Update Catalan Number.java

--- Library/Math/Catalan Number.java
+++ Library/Math/Catalan Number.java
@@ -1,6 +1,17 @@
 // Cat(0) = 0
 // Cat(n) = (C (2n , n)) / (n + 1)
 
+/*
+   1- Cn is the number of Dyck words[2] of length 2n.
+      A Dyck word is a string consisting of n X's and n Y's such that no initial segment of the string has more Y's than X's
+   2- Cn is the number of different ways n + 1 factors can be completely parenthesized 
+      (or the number of ways of associating n applications of a binary operator)
+   3- It follows that Cn is the number of full binary trees with n + 1 leaves
+   4- Cn is the number of monotonic lattice paths along the edges of a grid with n × n square cells, 
+      which do not pass above the diagonal
+   5- The number of triangles formed in a convex polygon is n and the number of different ways that this can be achieved is Cn
+   6- 
+*/
 static int cat [];
     public static void cat (int n)
     {
