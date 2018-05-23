15deec0bff1e78ed4d1aff78f8550bbd197fa9ea
omaryasser <oyaraouf@gmail.com>
2018-05-23 16:16:18 +0200

    Solved Live Archive Problem

---
+++ Live Archive Solutions/3930 - Drop the Triples.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+#include <bitset>
+
+#define REP(i,n) for(int i=0;i<(int)n;i++)
+#define REP1(i,j,n) for(int i=j;i<(int)n;i++)
+#define pb(x) push_back(x)
+#define fi first
+#define se second
+#define all(x) x.begin(),x.end()
+#define double long double
+#define mp(x,y) make_pair(x,y)
+
+typedef long long ll;
+
+using namespace std;
+
+const int N=10000+10;
+int mem[(int)(pow(3,13)+10)];
+
+int get(int *a){
+    int r=0,pw=1;
+    REP(i,13){
+        r+=a[i]*pw;
+        pw*=3;
+    }
+    return r;
+}
+int dp(int *a){
+    int msk=get(a);
+    int &r=mem[msk];
+    if(!msk)return 0;
+    if(r!=-1)return r;
+    int lst=-1;
+    REP(i,13)if(a[i])lst=i;
+    a[lst]--;
+    r=dp(a);
+    REP(i,lst+1)
+        REP1(j,i,lst+1){
+            if(a[i]>0&&a[j]>0&&a[i]+a[j]-(i==j?a[i]:0)>=2&&i+j>=lst){
+                a[i]--,a[j]--;
+                // if(lst==10&&i==1&&j==9){
+                //     printf("asdfa %d %d\n", a[i],a[j]);
+                //     REP(k,13)printf("%d\n", a[k]);
+                // }
+
+                r=max(r,1+dp(a));
+                // if(a[9]==0&&lst==2&&i==0&&j==2&&a[10]==0&&a[1]==0)printf("%d sadfasdf\n",r);
+                a[i]++,a[j]++;
+            }
+        }
+    a[lst]++;
+    return r;
+}
+
+int main(){
+    #ifndef ONLINE_JUDGE
+    freopen("in.txt","r",stdin);
+    #endif
+
+    memset(mem,-1,sizeof mem);
+    while(1){
+        int n;
+        scanf("%d",&n);
+        if(!n)break;
+        int c[2][13];
+        REP(i,2)REP(j,13)c[i][j]=0;
+        REP(i,n){
+            int x;
+            scanf("%d",&x);
+            x--;
+            c[i&1][x]++;
+            
+        }
+        pair<int,int>a,b;
+        REP(i,2)
+        REP(j,13){
+            if(!i)a.fi+=c[i][j]/3,c[i][j]%=3;
+            else b.fi+=c[i][j]/3,c[i][j]%=3;
+        }
+        // REP(i,13)c[0][i]=2;
+        a.se=dp(c[0]);
+        b.se=dp(c[1]);
+        // REP(i,13)printf("%d\n", c[0][i]);
+        // printf("%d %d\n", a.se,b.se);
+        if(a>b)
+            printf("1\n");
+        else if(b>a)
+            printf("2\n");
+        else printf("0\n");
+    }
+}
