cc02c3e3145ec092aed763cf1adce4fe492b244a
omaryasser <oyaraouf@gmail.com>
2018-05-29 01:27:58 +0200

    Added New Codes in New Library.

---
+++ New Library/Graphs/Bipartite/MCBM.cpp
@@ -0,0 +1,77 @@
+// Hopcroft O(E*sqrt(V))
+class BipGraph{
+    int n,m;
+    list<int>*adj;
+    int*pairU,*pairV,*dist;
+    int NIL=0,INF=1e9;
+public:
+    bool bfs(){
+        queue<int> Q;
+        for(int u=1;u<=n;u++){
+            if(pairU[u]==NIL){
+                dist[u]=0;
+                Q.push(u);
+            }
+            else dist[u]=INF;
+        }
+        dist[NIL]=INF;
+        while(!Q.empty()){
+            int u=Q.front();
+            Q.pop();
+            if(dist[u]<dist[NIL]){
+                list<int>::iterator i;
+                for(i=adj[u].begin();i!=adj[u].end();++i){
+                    int v=*i;
+                    if(dist[pairV[v]]==INF){
+                        dist[pairV[v]]=dist[u]+1;
+                        Q.push(pairV[v]);
+                    }
+                }
+            }
+        }
+        return (dist[NIL] != INF);
+    }
+    bool dfs(int u){
+        if (u!=NIL){
+            list<int>::iterator i;
+            for(i=adj[u].begin();i!=adj[u].end();++i){
+                int v=*i;
+                if(dist[pairV[v]]==dist[u]+1){
+                    if(dfs(pairV[v])== 1){
+                        pairV[v]=u;
+                        pairU[u]=v;
+                        return 1;
+                    }
+                }
+            }
+            dist[u]=INF;
+            return 0;
+        }
+        return 1;
+    }
+    int hopcroftKarp(){
+        pairU=new int[n+1];
+        pairV=new int[m+1];
+        dist=new int[n+1];
+        for (int u=0;u<=n;u++)
+            pairU[u]=NIL;
+        for (int v=0;v<=m;v++)
+            pairV[v]=NIL;
+        int result=0;
+        while(bfs()){
+            for(int u=1;u<=n;u++)
+                if (pairU[u]==NIL&&dfs(u)){
+                    result++;
+                }
+        }
+        return result;
+    }
+    BipGraph(int n,int m){
+        this->m =m;
+        this->n =n;
+        adj=new list<int>[n+1];
+    }
+    void addEdge(int u,int v){
+        adj[u].push_back(v);
+    }
+};
