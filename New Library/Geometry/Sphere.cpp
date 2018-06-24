83570b57787b53fec013d99e2ebc119bf01d8e3e
Omar Yasser <oyaraouf@gmail.com>
2018-06-24 15:49:02 +0200

    Update Sphere.cpp

--- New Library/Geometry/Sphere.cpp
+++ New Library/Geometry/Sphere.cpp
@@ -1,8 +1,8 @@
 // Distance between two Points on Sphere.
 double gcDistance(double pLat, double pLong,
 double qLat, double qLong, double radius) {
-	pLat *= PI / 180; pLong *= PI / 180;
 	// convert degree to radian
+	pLat *= PI / 180; pLong *= PI / 180;
 	qLat *= PI / 180; qLong *= PI / 180;
 	return radius * acos(cos(pLat)*cos(pLong)*cos(qLat)*cos(qLong) +
 	cos(pLat)*sin(pLong)*cos(qLat)*sin(qLong) +
