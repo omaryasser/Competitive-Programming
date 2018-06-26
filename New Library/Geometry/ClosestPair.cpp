019fa39efdfdef50fdc64c13164995a80b01c3cf
Omar Yasser <oyaraouf@gmail.com>
2018-06-26 12:08:47 +0200

    Update ClosestPair.cpp

--- New Library/Geometry/ClosestPair.cpp
+++ New Library/Geometry/ClosestPair.cpp
@@ -1,23 +1,3 @@
-#include <bits/stdc++.h>
-#define pb(x) push_back(x)
-#define bug cout<<"HERE"<<endl;
-#define SSTR( x ) static_cast< std::ostringstream & >( \
-        ( std::ostringstream() << std::dec << x ) ).str()
-#define clr(x,y) memset(x,y,sizeof(x))
-#define all(v) v.begin(),v.end()
-#define FOR(i,l) for(int i=0;i<l;++i)
-#define FOR1(i,s,l) for(int i=s;i<l;++i)
-#define FOR2(i,s) for(int i=s;i>=0;--i)
-#define fast 	ios_base::sync_with_stdio(0); cin.tie(0);
-#define inp freopen("input.txt", "r", stdin);
-#define out freopen("output.txt", "w", stdout);
-using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-inline int toInt(string s){int v; istringstream sin(s);sin>>v;return v;}
-inline ll toll(string s){ll v; istringstream sin(s);sin>>v;return v;}
-
 double EPS=1e-9;
 struct point{
 	double x,y;
