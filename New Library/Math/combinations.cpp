a954e84c03fed383c86c95a318b84927189a80f2
omaryasser <oyaraouf@gmail.com>
2018-08-06 14:42:05 +0200

    Added nCr function using inverse.

--- New Library/Math/combinations.cpp
+++ New Library/Math/combinations.cpp
@@ -1,3 +1,23 @@
+int MOD=1e9+7;
+const int MAX = 5 * 100000 + 1;
+ll fact [MAX];
+ll inv(ll x , ll mod){
+    ll r, y;
+    for(r = 1 , y = mod - 2 ; y != 0 ;x = x * x % mod, y>>=1)
+        if ((y & 1ll) == 1)
+            r = r * x % mod;
+    return r;
+}
+ll ncr (int n, int r) {
+    return ((ll)fact[n] * ((inv(fact[r], MOD) * inv(fact[n - r], MOD)) % MOD) % MOD) % MOD;
+}
+
+void factorial () {
+    fact[0] = fact[1] = 1;
+    for (int i = 2; i < MAX; i++)
+        fact[i] = ((ll)i * fact[i - 1] % MOD);
+}
+
 const int MAX=1000;
 ll comb[MAX][MAX];//init with -1
 ll nCr(int n,int k){
