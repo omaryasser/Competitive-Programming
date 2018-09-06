4c38016fcc11a9137ebc309eefa1c92b06c10b9f
Omar Yasser <oyaraouf@gmail.com>
2018-09-06 13:27:49 +0200

    Update SparseTable.cpp

--- New Library/Data Structures/SparseTable.cpp
+++ New Library/Data Structures/SparseTable.cpp
@@ -1,29 +1,51 @@
-struct ST{
-    int n,lg;
-    vector<vector<int> >tree;
-    vector<int>a,plog;
-    ST(int n,int *a){
-        this->n=n;
-        this->a=vector<int>(a,a+n);
-        lg=0;
-        while((1<<lg)<=n)lg++;
-        tree.assign(n,vector<int>(lg));
-        plog.assign(n+1,0);
-        int pw=2,curL=0;
-        REP(i,n+1){
-            if(i==pw)curL++,pw<<=1;
-            plog[i]=curL;
+struct ST {
+    int n, lg;
+    vector<vector<int> > treemax, treemin;
+    vector<int> a, plog;
+
+    void init(vector<int> a_) {
+        int n_ = (int)a_.size();
+        this->n = n_;
+        a.assign(n, 0);
+        for(int i = 0; i < n; i++) {
+            a[i] = a_[i];
         }
-        REP(i,n)tree[i][0]=a[i];
-        REP1(log,1,lg)
-            REP(i,n){
-                if(i+(1<<log)-1<n)
-                    tree[i][log]=__gcd(tree[i][log-1],tree[i+(1<<(log-1))][log-1]);
+        lg = 0;
+        while ((1 << lg) <= n)lg++;
+        treemax.assign(n, vector<int>(lg));
+        treemin.assign(n, vector<int>(lg));
+        plog.assign(n + 1, 0);
+        int pw = 2, curL = 0;
+        for (int i = 0; i < n + 1; i++) {
+            if (i == pw)curL++, pw <<= 1;
+            plog[i] = curL;
+        }
+        for (int i = 0; i < n; i++)
+            treemax[i][0] = a[i];
+        for (int log = 1; log < lg; log++)
+            for (int i = 0; i < n; i++) {
+                if (i + (1 << log) - 1 < n)
+                    treemax[i][log] = max(treemax[i][log - 1], treemax[i + (1 << (log - 1))][log - 1]);
+            }
+
+        for (int i = 0; i < n; i++)
+            treemin[i][0] = a[i];
+        for (int log = 1; log < lg; log++)
+            for (int i = 0; i < n; i++) {
+                if (i + (1 << log) - 1 < n)
+                    treemin[i][log] = min(treemin[i][log - 1], treemin[i + (1 << (log - 1))][log - 1]);
             }
     }
-    int q(int s,int e){
-        int d=plog[e-s+1];
-        if(s==e)return a[s];
-        return __gcd(tree[s][d],tree[e-(1<<d)+1][d]);
+
+    int qmax(int s, int e) {
+        int d = plog[e - s + 1];
+        if (s == e)return a[s];
+        return max(treemax[s][d], treemax[e - (1 << d) + 1][d]);
+    }
+
+    int qmin(int s, int e) {
+        int d = plog[e - s + 1];
+        if (s == e)return a[s];
+        return min(treemin[s][d], treemin[e - (1 << d) + 1][d]);
     }
 };
