36ed268d9f9d8823b874531f60191a494dcd4425
Omar Yasser <oyaraouf@gmail.com>
2018-09-10 15:11:58 +0200

    Create Suffix Automaton.cpp

---
+++ New Library/Strings/Suffix Automaton.cpp
@@ -0,0 +1,45 @@
+struct state {
+    int len, link;
+    map<char, int> next;
+};
+const int MAXLEN = 90001;
+state st[MAXLEN * 2];
+int sz, last;
+ll mem[MAXLEN << 1];
+ll INF = 1e16;
+
+void sa_init() {
+    st[0].len = 0;
+    st[0].link = -1;
+    sz++;
+    last = 0;
+}
+
+void sa_extend(char c) {
+    int cur = sz++;
+    st[cur].len = st[last].len + 1;
+    int p = last;
+    while (p != -1 && !st[p].next.count(c)) {
+        st[p].next[c] = cur;
+        p = st[p].link;
+    }
+    if (p == -1) {
+        st[cur].link = 0;
+    } else {
+        int q = st[p].next[c];
+        if (st[p].len + 1 == st[q].len) {
+            st[cur].link = q;
+        } else {
+            int clone = sz++;
+            st[clone].len = st[p].len + 1;
+            st[clone].next = st[q].next;
+            st[clone].link = st[q].link;
+            while (p != -1 && st[p].next[c] == q) {
+                st[p].next[c] = clone;
+                p = st[p].link;
+            }
+            st[q].link = st[cur].link = clone;
+        }
+    }
+    last = cur;
+}
